44a2c30703e88aa7b5c89f52a4ba64ef7b0cb44d
Ivan Gomez <ivangomezpd@gmail.com>
2025-02-11 10:42:49 +0100

    Update app.js
    realización de modificación de app.js

--- app.js
+++ app.js
@@ -1,41 +1,47 @@
-// mathExpressions.js
-
 function add(a, b) {
-  
+  return a + b;
 }
 
 function subtract(a, b) {
+  return a - b;
 }
 
 function multiply(a, b) {
+  return a * b;
 }
 
 function divide(a, b) {
- 
+  if (b === 0) {
+    throw new Error("No se puede dividir por cero.");
+  }
+  return a / b;
 }
 
 function modulus(a, b) {
-  
+  return a % b;
 }
 
 function exponent(a, b) {
-  
+  return Math.pow(a, b);
 }
 
 function squareRoot(a) {
- 
+  if (a < 0) {
+    throw new Error("No se puede calcular la raíz cuadrada de un número negativo.");
+  }
+  return Math.sqrt(a);
 }
 
 function cube(a) {
-  
+  return Math.pow(a, 3);
 }
 
 function absolute(a) {
-  
+  return Math.abs(a);
 }
 
 function sine(degrees) {
-  
+  return Math.sin((degrees * Math.PI) / 180);
 }
 
 module.exports = {
@@ -48,5 +54,5 @@ module.exports = {
   squareRoot,
   cube,
   absolute,
-  sine
+  sine,
 };
