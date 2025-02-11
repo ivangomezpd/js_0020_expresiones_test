48adeb3b2c597ac43adfd13bc46168920a525371
Ivan Gomez <ivangomezpd@gmail.com>
2025-02-11 10:53:26 +0100

    Update logicFunctions.js
    respuesta a las codificaciones de expresiones

--- logicFunctions.js
+++ logicFunctions.js
@@ -1,43 +1,41 @@
 function isEven(num) {
-    
+    return num % 2 === 0;
 }
 
 function isGreater(a, b) {
-    
+    return a > b;
 }
 
 function isLessOrEqual(a, b) {
-    
+    return a <= b;
 }
 
 function isInRange(value, min, max) {
-    
+    return value >= min && value <= max;
 }
 
 function areEqual(a, b) {
-    
+    return a === b;
 }
 
 function areNotEqual(a, b) {
-    
+    return a !== b;
 }
 
 function eitherOr(a, b) {
-   
-        
+    return a || b; // Devuelve true si al menos uno es true
 }
 
 function neitherNor(a, b) {
-    
+    return !a && !b; // Devuelve true si ambos son false
 }
 
 function bothAnd(a, b) {
-    
+    return a && b; // Devuelve true si ambos son true
 }
 
 function exclusiveOr(a, b) {
-   
-        
+    return !!(a ^ b); // XOR lógico: true solo si uno es true y el otro false
 }
 
 module.exports = {
